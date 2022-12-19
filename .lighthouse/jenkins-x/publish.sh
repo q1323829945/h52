948532dc2326d8aae0067c20ccecbf4c83d655ef
lizheng2 <lizheng2@sunline.cn>
2022-12-19 17:27:13 +0800

    1

--- .lighthouse/jenkins-x/publish.sh
+++ .lighthouse/jenkins-x/publish.sh
@@ -3,7 +3,7 @@
  # @Author: lizheng2 lizheng2@sunline.cn
  # @Date: 2022-12-19 10:18:13
  # @LastEditors: lizheng2 lizheng2@sunline.cn
- # @LastEditTime: 2022-12-19 10:18:24
+ # @LastEditTime: 2022-12-19 15:32:42
  # @FilePath: \jx3-pipeline-catalog\packs\javascript-ui-nginx\.lighthouse\jenkins-x\publish.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
